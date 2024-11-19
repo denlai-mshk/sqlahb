bce0e951080381f22c2928a6f9fe8c9ef028853e
Den Lai <denlai@microsoft.com>
2024-11-19 18:52:00 +0800

    Initial commit

---
+++ enablesqlahb.ps1
@@ -0,0 +1,239 @@
+param (
+    [string]$Mode = "write"  # Default to "read" if no parameter is provided
+)
+
+# Ensure the required modules are imported
+if (-not (Get-Module -ListAvailable -Name Az.Accounts)) {
+    Install-Module -Name Az.Accounts -AllowClobber -Force
+}
+Import-Module Az.Accounts
+
+if (-not (Get-Module -ListAvailable -Name Az.Sql)) {
+    Install-Module -Name Az.Sql -AllowClobber -Force
+}
+Import-Module Az.Sql
+<#
+# Ensure proper login
+$TenantId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"
+try {
+    Connect-AzAccount -TenantId $TenantId -ErrorAction Stop
+} catch {
+    Write-Output "Failed to authenticate. Please ensure you are logged in with the correct account."
+    exit
+}
+#>
+# Import the list of subscriptions from sublist.txt
+$subscriptions = Import-Csv -Path "sublist.txt" -Delimiter ',' -Header "SubscriptionName", "SubscriptionId" | Select-Object -Skip 1
+
+# Initialize the output files
+$outputFile = "findallsqlsvr.txt"
+$ahbonlyFile = "findahbonly.txt"
+$disabledFile = "resultpaygo.txt"
+if (Test-Path $outputFile) {
+    Remove-Item $outputFile
+}
+New-Item -Path $outputFile -ItemType File
+
+if (Test-Path $ahbonlyFile) {
+    Remove-Item $ahbonlyFile
+}
+New-Item -Path $ahbonlyFile -ItemType File
+
+if (Test-Path $disabledFile) {
+    Remove-Item $disabledFile
+}
+New-Item -Path $disabledFile -ItemType File
+
+# Function to log discovery details
+function Log-Discovery {
+    param (
+        [string]$SqlType,
+        [string]$LicenseType,
+        [string]$vCores,
+        [string]$StorageinGB,
+        [string]$SubscriptionId,
+        [string]$Region,
+        [string]$ResourceGroup,
+        [string]$SqlName,
+        [string]$ResourceId
+    )
+        $logEntry = "$SqlType, $LicenseType, $vCores, $StorageinGB, $SubscriptionId, $Region, $ResourceGroup, $SqlName, $ResourceId"
+    Add-Content -Path $outputFile -Value $logEntry
+}
+
+function Log-AHBOnly {
+    param (
+        [string]$SqlType,
+        [string]$LicenseType,
+        [string]$vCores,
+        [string]$StorageinGB,
+        [string]$SubscriptionId,
+        [string]$Region,
+        [string]$ResourceGroup,
+        [string]$SqlName,
+        [string]$ResourceId
+    )
+        $logEntry = "$SqlType, $LicenseType, $vCores, $StorageinGB, $SubscriptionId, $Region, $ResourceGroup, $SqlName, $ResourceId"
+    Add-Content -Path $ahbonlyFile -Value $logEntry
+}
+
+# Function to log disabled AHB details
+function Log-Disabled {
+    param (
+        [string]$SqlType,
+        [string]$LicenseType,
+        [string]$vCores,
+        [string]$StorageinGB,
+        [string]$SubscriptionId,
+        [string]$Region,
+        [string]$ResourceGroup,
+        [string]$SqlName,
+        [string]$ResourceId
+    )
+    $logEntry = "$SqlType, $LicenseType, $vCores, $StorageinGB, $SubscriptionId, $Region, $ResourceGroup, $SqlName, $ResourceId"
+    Add-Content -Path $disabledFile -Value $logEntry
+}
+Log-Discovery -SqlType "SqlType" -LicenseType "LicenseType" -vCores "vCores" -StorageinGB "StorageinGB"  -SubscriptionId "SubscriptionId" -Region "Region" -ResourceGroup "ResourceGroup"  -SqlName "SqlName" -ResourceId "ResourceId"
+Log-AHBOnly -SqlType "SqlType" -LicenseType "LicenseType" -vCores "vCores" -StorageinGB "StorageinGB"  -SubscriptionId "SubscriptionId" -Region "Region" -ResourceGroup "ResourceGroup"  -SqlName "SqlName" -ResourceId "ResourceId"
+Log-Disabled -SqlType "SqlType" -LicenseType "LicenseType" -vCores "vCores" -StorageinGB "StorageinGB"  -SubscriptionId "SubscriptionId" -Region "Region" -ResourceGroup "ResourceGroup"  -SqlName "SqlName" -ResourceId "ResourceId"
+
+Write-Output "$(Get-Date -Format HH:mm:ss) Job started"
+# Loop through each subscription
+foreach ($subscription in $subscriptions) {
+    Write-Output "$(Get-Date -Format HH:mm:ss) Processing subscriptions: $($subscription.SubscriptionName)"
+    $SubscriptionId = $subscription.SubscriptionId
+    $SubscriptionName = $subscription.SubscriptionName
+
+    # Check if SubscriptionId or SubscriptionName is empty
+    if ([string]::IsNullOrEmpty($SubscriptionId) -or [string]::IsNullOrEmpty($SubscriptionName)) {
+        Write-Output "Error: Missing SubscriptionId or SubscriptionName"
+        break
+    }
+
+    try {
+        # Debug output to check the subscription details
+        Write-Output "Setting context for Subscription: $SubscriptionName ($SubscriptionId)"
+
+        # Set the current subscription context
+        Select-AzSubscription -SubscriptionId $SubscriptionId -ErrorAction Stop
+
+        # Discover SQL Instance Pools
+        Write-Output "$(Get-Date -Format HH:mm:ss) Processing Get-AzSqlInstancePool"
+        $sqlInstancePools = Get-AzSqlInstancePool
+        foreach ($pool in $sqlInstancePools) {
+            if ([string]::IsNullOrEmpty($pool.LicenseType)) {
+                continue
+            }            
+            $vCores = $pool.VCores
+            $storageInGB = "0" # no storage attributes in instance pools object
+            Log-Discovery -SqlType "sqlmipool" -LicenseType $pool.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $pool.Location -ResourceGroup $pool.ResourceGroupName -SqlName $pool.InstancePoolName -ResourceId $pool.Id
+
+            if ($pool.LicenseType -eq "LicenseIncluded") {
+                Log-AHBOnly -SqlType "sqlmipool" -LicenseType $pool.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $pool.Location -ResourceGroup $pool.ResourceGroupName -SqlName $pool.InstancePoolName -ResourceId $pool.Id
+            }
+            
+            if ($Mode -eq "write" -and $pool.LicenseType -eq "LicenseIncluded") {
+                # Disable Azure Hybrid Benefit
+                Write-Output "$(Get-Date -Format HH:mm:ss) Processing Set-AzSqlInstancePool"
+                Set-AzSqlInstancePool -ResourceGroupName $pool.ResourceGroupName -Name $pool.InstancePoolName -LicenseType "BasePrice"
+                
+                # Log the successful disablement
+                Log-Disabled -SqlType "sqlmipool" -LicenseType "BasePrice" -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $pool.Location -ResourceGroup $pool.ResourceGroupName -SqlName $pool.InstancePoolName -ResourceId $pool.Id
+            }
+        }       
+        # Discover SQL Managed Instances
+        Write-Output "$(Get-Date -Format HH:mm:ss) Processing Get-AzSqlInstance"
+        $sqlManagedInstances = Get-AzSqlInstance
+        foreach ($mi in $sqlManagedInstances) {
+            if ([string]::IsNullOrEmpty($mi.LicenseType)) {
+                continue
+            } 
+            # Check if the managed instance is affiliated with any pool instance using $mi attributes
+            if ($mi.InstancePoolName) {
+                continue
+            }
+                     
+            $vCores = $mi.VCores
+            $storageInGB = $mi.StorageSizeInGB
+            Log-Discovery -SqlType "sqlmi" -LicenseType $mi.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $mi.Location -ResourceGroup $mi.ResourceGroupName -SqlName $mi.ManagedInstanceName -ResourceId $mi.Id
+
+            if ($mi.LicenseType -eq "LicenseIncluded") {
+                Log-AHBOnly -SqlType "sqlmi" -LicenseType $mi.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $mi.Location -ResourceGroup $mi.ResourceGroupName -SqlName $mi.ManagedInstanceName -ResourceId $mi.Id
+            }
+            
+            if ($Mode -eq "write" -and $mi.LicenseType -eq "LicenseIncluded") {
+                # Disable Azure Hybrid Benefit
+                Write-Output "$(Get-Date -Format HH:mm:ss) Processing Set-AzSqlInstance"
+                Set-AzSqlInstance -ResourceGroupName $mi.ResourceGroupName -Name $mi.ManagedInstanceName -LicenseType "BasePrice" -Force
+                
+                # Log the successful disablement
+                Log-Disabled -SqlType "sqlmi" -LicenseType "BasePrice" -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $mi.Location -ResourceGroup $mi.ResourceGroupName -SqlName $mi.ManagedInstanceName -ResourceId $mi.Id
+            }
+
+        }      
+
+        # Discover SQL Servers
+        Write-Output "$(Get-Date -Format HH:mm:ss) Processing Get-AzSqlServer"
+        $sqlServers = Get-AzSqlServer
+        foreach ($server in $sqlServers) {
+            # Discover SQL Databases for each server
+            $sqlDatabases = Get-AzSqlDatabase -ResourceGroupName $server.ResourceGroupName -ServerName $server.ServerName
+            foreach ($db in $sqlDatabases) {
+                # Exclude databases affiliated with SQL elastic pools
+                if (-not $db.ElasticPoolName) {
+                    if ([string]::IsNullOrEmpty($db.LicenseType)) {
+                        continue
+                    }
+                    $vCores = $db.Capacity
+                    $storageInGB = [math]::Round($db.MaxSizeBytes / 1GB, 2)
+                    Log-Discovery -SqlType "sqldb" -LicenseType $db.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $db.Location -ResourceGroup $db.ResourceGroupName  -SqlName $db.DatabaseName -ResourceId $db.ResourceId
+
+                    if ($db.LicenseType -eq "LicenseIncluded") {
+                        Log-AHBOnly -SqlType "sqldb" -LicenseType $db.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $db.Location -ResourceGroup $db.ResourceGroupName  -SqlName $db.DatabaseName -ResourceId $db.ResourceId
+                    }
+                            
+                    if ($Mode -eq "write" -and $db.LicenseType -eq "LicenseIncluded") {
+                        # Disable Azure Hybrid Benefit
+                        Write-Output "$(Get-Date -Format HH:mm:ss) Processing Set-AzSqlDatabase"
+                        Set-AzSqlDatabase -ResourceGroupName $db.ResourceGroupName -ServerName $db.ServerName -DatabaseName $db.DatabaseName -LicenseType "BasePrice"
+                        
+                        # Log the successful disablement
+                        Log-Disabled -SqlType "sqldb" -LicenseType "BasePrice" -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $db.Location -ResourceGroup $db.ResourceGroupName  -SqlName $db.DatabaseName -ResourceId $db.ResourceId
+                    }
+                }
+            }
+            # Discover SQL Elastic Pools
+            Write-Output "$(Get-Date -Format HH:mm:ss) Processing Get-AzSqlElasticPool"
+            $sqlElasticPools = Get-AzSqlElasticPool -ResourceGroupName $server.ResourceGroupName -ServerName $server.ServerName
+            foreach ($pool in $sqlElasticPools) {
+                $vCores = $pool.Capacity
+                $storageInGB = [math]::Round($pool.StorageMB / 1024, 2)
+                Log-Discovery -SqlType "sqlpool" -LicenseType $pool.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $pool.Location -ResourceGroup $pool.ResourceGroupName -SqlName $pool.ElasticPoolName -ResourceId $pool.ResourceId
+
+                if ($pool.LicenseType -eq "LicenseIncluded") {
+                    Log-AHBOnly -SqlType "sqlpool" -LicenseType $pool.LicenseType -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $pool.Location -ResourceGroup $pool.ResourceGroupName -SqlName $pool.ElasticPoolName -ResourceId $pool.ResourceId
+                }
+                                  
+                if ($Mode -eq "write" -and $pool.LicenseType -eq "LicenseIncluded") {
+                    # Disable Azure Hybrid Benefit
+                    Write-Output "$(Get-Date -Format HH:mm:ss) Processing Set-AzSqlElasticPool"
+                    Set-AzSqlElasticPool -ResourceGroupName $pool.ResourceGroupName -ServerName $pool.ServerName -ElasticPoolName $pool.ElasticPoolName -LicenseType "BasePrice"
+                    
+                    # Log the successful disablement
+                    Log-Disabled -SqlType "sqlpool" -LicenseType "BasePrice" -vCores $vCores -StorageinGB $storageInGB -SubscriptionId $SubscriptionId -Region $pool.Location -ResourceGroup $pool.ResourceGroupName -SqlName $pool.ElasticPoolName -ResourceId $pool.ResourceId
+                }
+            }
+
+                   
+            
+        }
+
+
+
+
+    } catch {
+        Write-Output "Failed to set context for subscription $SubscriptionId. Error: $_"
+    }
+}
+
+Write-Output "$(Get-Date -Format HH:mm:ss) Discovery and disablement completed. Check the discovery.txt, ahbonly.txt and disabledsql.txt files for details."
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Steps to check Azure Hybrid Benefit for all SQL servers in your subscription
+This Powershell scripts are designed for changing the license type from Azure Hybrid Benefit to Pay as You Go.
+
+
+##  Step 1: Install the Azure PowerShell Module
+    If you haven't already installed the Azure PowerShell module, you can do so using the following command:
+```
+    Install-Module -Name Az.Accounts -AllowClobber -Force
+    Install-Module -Name Az.Sql -AllowClobber -Force
+```
+
+##  Step 3: Verify the Installation
+You can verify that the module is installed and the cmdlets are available by listing the commands:
+```
+Get-Module -ListAvailable -Name Az.Sql
+Get-Module -ListAvailable -Name Az.Accounts
+```   
+
+##  Step 3: Verify the Installation
+Sign on with your browser, you need to have Azure Subscription Owner role
+
+```
+Connect-AzAccount -TenantId "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"
+``` 
+
+##  Step 4: Modify the sublist.txt
+You can change all SQL servers deployed in multiple subscriptions with one go. Edit the **sublist.txt** and place your subscription name and id under the 1st header row "SubscriptionName", "SubscriptionId"
+```
+"SubscriptionName", "SubscriptionId"
+your-sub-name, xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx
+```  
+
+##  Step 5: Execute the checksqlahb.ps1 for checking
+Open powershell, locate to the script folder, execute  [checksqlahb](\checksqlahb.ps1)
+``` 
+.\checksqlahb.ps1
+``` 
+
+Set license change may takes 1-2 minutues for each SQL Server approximately. Please be patient if your subscriptions have a lot of SQL Server.
+
+After you see "Discovery and disablement completed", you can check the **findallsqlsvr.txt** for all SQL Servers discovery. You also can check the findahbonly.txt which only list out the SQL Server with Azure Hybrid Benefit enabled.    
+
+##  Step 6: Execute the confirmsqlahb.ps1 for change all Azure Hybrid Benegfit to PayGO
+If you confirm to change all the SQL Servers listed in the **findallsqlsvr.txt** from Azure Hybrid Benefit to Pay as you go, then execute  [confirmsqlahb](\confirmsqlahb.ps1)
+
+``` 
+.\confirmsqlahb.ps1
+``` 
+
+After you see "Discovery and disablement completed", you can check the **resultpaygo.txt** for the result
